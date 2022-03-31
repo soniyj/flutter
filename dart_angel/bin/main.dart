be60ac437e77df58751c33a1d746a547080af6c7
soniyj <mark.punk182@gmail.com>
2022-03-31 15:34:52 +0200

    Dart FTP Test

--- dart_angel/bin/main.dart
+++ dart_angel/bin/main.dart
@@ -1,6 +1,9 @@
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_framework/http.dart';
 
+import 'dart:io';
+import 'package:ftpconnect/ftpConnect.dart';
+
 main() async {
   var app = Angel();
   var http = AngelHttp(app);
@@ -8,6 +11,8 @@ main() async {
   app.get('/', (req, res) => res.write('Hello, world!'));
 
   app.post('/tor_onions', echoPost);
+
+  app.get('/zxc', ftptest);
   
   // await http.startServer('localhost', 3000);
   await http.startServer('0.0.0.0', 3000);
@@ -41,5 +46,26 @@ oldtest(res, req) async {
       // Prints the whole object
       res.write(req.bodyAsMap);
       res.writeln();
+  }
+}
+
+ftptest(res, req) async {
+  // await req.parseBody();
+
+  FTPConnect ftpConnect = FTPConnect('ftp.server', port: 1587, user: 'username', pass:'password');
+  try {
+      String fileName = 'FILE.TXT';
+      // res.write('Connecting...');
+      print('Connecting...');
+      await ftpConnect.connect();
+      print('Downloading...');
+      await ftpConnect.downloadFile(fileName, File('FILE.TXT'));
+      // res.write('Connecting...');
+      print('Disconect...');
+      await ftpConnect.disconnect();
+  } catch (e) {
+      // res.write('Ops: Error');
+      print("Error");
+      print(e);
   }
 }
--- dart_angel/bin/server.dart
+++ dart_angel/bin/server.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+// import 'dart:html';
+
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_framework/http.dart';
 
@@ -8,10 +12,13 @@ main() async {
   app.get('/', (req, res) => res.write('Hello, world!'));
 
   app.post('/tor_onions', echoPost);
+
+  app.get('/qwerty', qwertyGet);
+  app.post('/qwerty', qwertyPost);
   
   // await http.startServer('localhost', 3000);
   await http.startServer('0.0.0.0', 3000);
-  print("Angel server listening at ${http.uri}");
+  print('Angel server listening at ${http.uri}');
 }
 
 echoPost(RequestContext<dynamic> req, ResponseContext<dynamic> res) async {
@@ -41,5 +48,51 @@ oldtest(res, req) async {
       // Prints the whole object
       res.write(req.bodyAsMap);
       res.writeln();
+  }
+}
+
+qwertyGet(RequestContext<dynamic> req, ResponseContext<dynamic> res) async {
+  // var request = await HttpClient().getUrl(Uri.parse(url)); // produces a request object
+  // var response = await request.close(); // sends the request
+  
+  // // transforms and prints the response
+  // await for (var contents in response.transform(Utf8Decoder())) {
+  //   print(contents);
+  // }
+  // print('marco');
+
+  await req.parseBody();
+
+  print(req);
+
+  var name = req.bodyAsMap['name'] as String;
+  var user = req.bodyAsMap['user'] as String;
+
+  print(name);
+  print(user);
+}
+
+// OK it works
+qwertyPost(RequestContext<dynamic> req, ResponseContext<dynamic> res) async {
+  var url = 'http://your.amazing.api/graphql'; //OR Rest
+
+  Map jsonData = {
+    'data_1': 'ABC1234567',
+    'data_2': 'TYPO'
+  };
+
+  var request = await  HttpClient().postUrl(Uri.parse(url));
+  request.headers.contentType = ContentType.json;
+  request.write(jsonEncode(jsonData));
+  var response = await request.close(); // sends the request
+
+  // transforms and prints the response
+  await for (var contents in response.transform(Utf8Decoder())) {
+    print(contents);
+    final body = jsonDecode(contents);
+    print(body);
+    if (body['status']=='SUCCESS') {
+      print('YEs');
+    }
   }
 }
